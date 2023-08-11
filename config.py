8fedc1b4346f3978da3c8f089472b72af3812883
JL Reyes <joseluisreyes43@gmail.com>
2023-08-11 23:10:41 +0800

    created some catmon templates and added the first catmon initialization + display stats & moves

--- config.py
+++ config.py
@@ -14,4 +14,44 @@ TYPE_EFFECTIVENESS = {
     "Normal": {},
 }
 
-# LOG_LEVELS = {"Informational", "Battle"}
+CATMON_TEMPLATES = {
+    "mugi": {
+        "name": "mugi",
+        "type": "Fire",
+        "health": 100,
+        "max_health": 100,
+        "moves": [],
+        "atk": 15,
+        "def_": 10,
+        "sp_atk": 20,
+        "sp_def": 10,
+        "speed": 12,
+        "level": 1,
+    },
+    "buwie": {
+        "name": "buwie",
+        "type": "Water",
+        "health": 100,
+        "max_health": 100,
+        "moves": [],
+        "atk": 12,
+        "def_": 12,
+        "sp_atk": 18,
+        "sp_def": 12,
+        "speed": 11,
+        "level": 1,
+    },
+    "raffy": {
+        "name": "raffy",
+        "type": "Grass",
+        "health": 100,
+        "max_health": 100,
+        "moves": [],
+        "atk": 13,
+        "def_": 15,
+        "sp_atk": 17,
+        "sp_def": 15,
+        "speed": 10,
+        "level": 1,
+    },
+}
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from catmon import Catmon
+from config import CATMON_TEMPLATES
 
 print("Welcome to the world of catmon!")
 
 catmon_is_picked: bool = False
+player_catmon_choice: str = ""
 
 while not catmon_is_picked:
     print("Choose a catmon:")
-    user_catmon_choice = input("Mugi, Buwie, Raffy\n")
-    print(f"You've selected {user_catmon_choice}!")
-    if user_catmon_choice == "Mugi":
-        print(f"{user_catmon_choice} is a fire type catmon that will fuck you up")
+    player_catmon_choice = (input("mugi, buwie, raffy\n")).lower()
+    print(f"You've selected {player_catmon_choice}!")
+    if player_catmon_choice == "mugi":
+        print(f"{player_catmon_choice} is a fire type catmon that will fuck you up")
         catmon_is_picked = True
-    elif user_catmon_choice == "Buwie":
+    elif player_catmon_choice == "buwie":
         print(
-            f"{user_catmon_choice} is a water type catmon that will give u wet kisses"
+            f"{player_catmon_choice} is a water type catmon that will give u wet kisses"
         )
         catmon_is_picked = True
-    elif user_catmon_choice == "Raffy":
+    elif player_catmon_choice == "raffy":
         print(
-            f"{user_catmon_choice} is a grass type catmon that will ask u to touch some grass"
+            f"{player_catmon_choice} is a grass type catmon that will ask u to touch some grass"
         )
         catmon_is_picked = True
     else:
         print("That catmon isn't in the choices!")
+
+# mypy does not like unpacking
+player_catmon: Catmon = Catmon(**CATMON_TEMPLATES[player_catmon_choice])  # type: ignore
+player_catmon.display_stats()
+player_catmon.display_moves()
