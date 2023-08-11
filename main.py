28c170f94aff7e6483b112cf5840329a2bca9728
JL Reyes <joseluisreyes43@gmail.com>
2023-08-12 00:43:52 +0800

    instanciated the enemy catmon

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print("Welcome to the world of catmon!")
 
 catmon_is_picked: bool = False
 player_catmon_choice: str = ""
+enemy_catmon_choice: str = ""
 
 while not catmon_is_picked:
     print("Choose a catmon:")
@@ -12,16 +13,19 @@ while not catmon_is_picked:
     print(f"You've selected {player_catmon_choice}!")
     if player_catmon_choice == "mugi":
         print(f"{player_catmon_choice} is a fire type catmon that will fuck you up")
+        enemy_catmon_choice = "buwie"
         catmon_is_picked = True
     elif player_catmon_choice == "buwie":
         print(
             f"{player_catmon_choice} is a water type catmon that will give u wet kisses"
         )
+        enemy_catmon_choice = "raffy"
         catmon_is_picked = True
     elif player_catmon_choice == "raffy":
         print(
             f"{player_catmon_choice} is a grass type catmon that will ask u to touch some grass"
         )
+        enemy_catmon_choice = "mugi"
         catmon_is_picked = True
     else:
         print("That catmon isn't in the choices!")
@@ -45,3 +49,10 @@ while not nickname_is_given:
     else:
         print("That's not a valid input!")
         nickname_is_given = False
+
+print("Let's have our first battle!")
+
+# mypy does not like unpacking
+enemy_catmon: Catmon = Catmon(**CATMON_TEMPLATES[enemy_catmon_choice])  # type: ignore
+
+print(f"Your Catmon: {player_catmon.nickname} vs Enemy Catmon: {enemy_catmon.nickname}")
